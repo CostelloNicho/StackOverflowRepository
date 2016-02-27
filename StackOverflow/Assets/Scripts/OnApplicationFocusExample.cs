607d8dbc8627501f27c7b3053c8ac272d03221b5
CostelloNicho <nicholasjcostello@gmail.com>
2016-02-27 08:57:49 -0500

    Added application focus test

---
+++ StackOverflow/Assets/Scripts/OnApplicationFocusExample.cs
@@ -0,0 +1,26 @@
+﻿#region Directives
+
+using UnityEngine;
+
+#endregion
+
+public class OnApplicationFocusExample : MonoBehaviour
+{
+    protected void OnApplicationFocus(bool focusStatus)
+    {
+        if (focusStatus)
+            OnFocus();
+        else
+            OnFocusLost();
+    }
+
+    private void OnFocus()
+    {
+        Debug.Log("Game now active on device");
+    }
+
+    private void OnFocusLost()
+    {
+        Debug.Log("Game now inactive");
+    }
+}
